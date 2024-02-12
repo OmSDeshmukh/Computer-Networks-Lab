3027e5561f61947b85c981c14daa37a6bf99b419
Om Deshmukh <omyash1111@gmail.com>
2024-02-13 00:15:02 +0530

    Solved q2

--- Lab6/client.py
+++ Lab6/client.py
@@ -9,7 +9,7 @@ client_name = "210010033_client"
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect the socket to the port where the server is listening
-server_address = ('localhost', 10001)
+server_address = ('localhost', 10000)
 print(f"connecting to {server_address[0]} port {server_address[1]}")
 sock.connect(server_address)
 
@@ -23,22 +23,21 @@ try:
         print("data sent")
 
         data = sock.recv(1024)
-        if data:
-            received_list = json.loads(data.decode())
 
-            if received_list[1] > 100 or received_list[1] < 1:
-                print("Integer sent out of range")
-                print("Closing connection")
-                break  # Exit the loop if integer is out of range
-            else:
-                print(f"Server Name: {received_list[0]}")
-                print(f"Random number from server: {received_list[1]}")
-                i = int(input("Press 1 to continue sending messages\nPress 0 to stop sending messages "))
-                if i == 0:
-                    break  # Exit the loop if user chooses to stop sending messages
+        received_list = json.loads(data.decode())
+        if received_list[1] > 100 or received_list[1] < 1:
+            print("Integer sent out of range")
+            print("Closing connection")
+            break  # Exit the loop if integer is out of range
         else:
-            print("No data received")
-            break  # Exit the loop if no data received
-finally:
-    print('closing socket')
+            print(f"Server Name: {received_list[0]}")
+            print(f"Random number from server: {received_list[1]}")
+            i = int(input("Press 1 to continue sending messages\nPress 0 to stop sending messages "))
+            if i == 0:
+                break  # Exit the loop if user chooses to stop sending messages
+except:
+    print('Closing socket due to error')
     sock.close()
+    
+print('closing socket')
+sock.close()
--- Lab6/server.py
+++ Lab6/server.py
@@ -1,56 +1,57 @@
 import socket
 import sys
 import json
-import random
-
-def handle_client(connection, server_name):
-    # receive data
-    data = connection.recv(1024)
-    if data:
-        received_list = json.loads(data.decode())
-        if received_list[1] > 100 or received_list[1] < 1:
-            print("Server Closing")
-            return 11
-
-        print(f"Client Name: {received_list[0]}")
-        print(f"Server Name: {server_name}")
-
-        random_number = random.randint(1, 100)
-
-        print(f"Client's random number: {received_list[1]}")
-        total_sum = received_list[1] + random_number
-        print(f"Sum of random numbers is {total_sum}")
-
-        print(f"Server's random number: {random_number}")
-        print('sending data back to the client')
-        data_list = json.dumps([server_name, random_number])
-        data = data_list.encode()
-        connection.sendall(data)
-    else:
-        print("No data received")
-        return 11
+import random    
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Bind the socket to the port
-server_address = ('localhost', 10001)
+server_address = ('localhost', 10000)
 server_name = "210010033_server"
 
-print('starting up on %s port %s' % server_address)
+print(' Server starting up on %s port %s' % server_address)
 sock.bind(server_address)
 
+# put socket in listening mode
 # Listen for incoming connections
 sock.listen(5)
 print('waiting for a connection')
-connection, client_address = sock.accept()
-print("Client connected")
-try:
-    while True:
-        # Wait for a connection
-        if(handle_client(connection, server_name)==11):
-            break
-except KeyboardInterrupt:
-    print("Server shutting down")
-finally:
-    sock.close()
+
+while True:
+    # connecting to the client
+    connection, client_address = sock.accept()
+    print("Client connected")
+    
+    try:
+    # Wait for a connection
+        while True:
+            data = connection.recv(1024)
+            received_list = json.loads(data.decode())
+            if received_list[1] > 100 or received_list[1] < 1:
+                print("Integer sent out of range")
+                print("Closing connection")
+                break
+
+            print(f"Client Name: {received_list[0]}")
+            print(f"Server Name: {server_name}")
+
+            random_number = random.randint(1, 100)
+
+            print(f"Client's random number: {received_list[1]}")
+            total_sum = received_list[1] + random_number
+            print(f"Sum of random numbers is {total_sum}")
+
+            print(f"Server's random number: {random_number}")
+            print('sending data back to the client')
+            data_list = json.dumps([server_name, random_number])
+            data = data_list.encode()
+            connection.sendall(data)
+    except Exception as e:
+        print("Connection closed by client",e)
+        break
+    finally:
+        connection.close()
+    
+print("Server Closing")
+sock.close()
