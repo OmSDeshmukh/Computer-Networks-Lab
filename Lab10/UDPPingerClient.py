c4a80f73a6f16f5548b617d601ae7de2ded45912
Om Deshmukh <omyash1111@gmail.com>
2024-03-14 22:10:46 +0530

    Started Lab 10

---
+++ Lab10/UDPPingerClient.py
@@ -0,0 +1,32 @@
+# We will need the following module to generate randomized lost packets 
+import random 
+from socket import * 
+import time
+
+# Create a UDP socket 
+# Notice the use of SOCK_DGRAM for UDP packets 
+
+clientSocket = socket(AF_INET, SOCK_DGRAM) 
+
+# Assign IP address and port number to socket
+address =  ('127.0.0.1', 12002)
+# clientSocket.connect(address) 
+clientSocket.settimeout(1)
+
+
+i = 0
+while i<10:
+    try:
+        s = time.time()
+        message = f"Ping {i} {s}"
+        clientSocket.sendto(message.encode('utf_8'), address)
+        message_received, address = clientSocket.recvfrom(1024) 
+        e = time.time()
+        print("Message Received: ",message_received.decode())
+        print(f"RTT: {(e-s)} seconds")
+        print("")
+    except timeout:
+        print("Request timed out")
+        print("")
+    finally:
+        i+=1
